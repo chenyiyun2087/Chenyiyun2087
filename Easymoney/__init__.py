ed8e0d23080aceb6ccc8923c720cad2c630aa2ab
chenyiyun <chenyiyun2087@gmail.com>
2026-02-05 12:08:33 +0800

    update column fields name

--- Easymoney/__init__.py
+++ Easymoney/__init__.py
@@ -1,6 +1,13 @@
-"""Eastmoney data access package."""
-
 from .EastmoneyController import EastmoneyController
-from .LongShort_scanner import DuokongSnapshot, fetch_duokong_snapshot
+from .duokong_batch import run_batch
+from .duokong_scanner import DuokongSnapshot, fetch_duokong_snapshot
+from .duokong_storage import save_snapshots_to_mysql
 
-__all__ = ["EastmoneyController", "DuokongSnapshot", "fetch_duokong_snapshot"]
+__all__ = ["EastmoneyController"]
+__all__ = [
+    "EastmoneyController",
+    "DuokongSnapshot",
+    "fetch_duokong_snapshot",
+    "run_batch",
+    "save_snapshots_to_mysql",
+]
