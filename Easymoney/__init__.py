4c340ed53d8dc8d985d902078c7e218aef7bdae9
chenyiyun <chenyiyun2087@gmail.com>
2026-02-05 11:45:36 +0800

    update column fields name

--- Easymoney/__init__.py
+++ Easymoney/__init__.py
@@ -1,6 +1,6 @@
 """Eastmoney data access package."""
 
 from .EastmoneyController import EastmoneyController
-from .duokong_scanner import DuokongSnapshot, fetch_duokong_snapshot
+from .LongShort_scanner import DuokongSnapshot, fetch_duokong_snapshot
 
 __all__ = ["EastmoneyController", "DuokongSnapshot", "fetch_duokong_snapshot"]
