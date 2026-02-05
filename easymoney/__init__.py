33ee56b90709c46b4969223ca390a28def97ca43
chenyiyun2087 <chenyiyun2087@gmail.com>
2026-02-05 10:19:01 +0800

    Merge eastmoney scanners into easymoney

--- easymoney/__init__.py
+++ easymoney/__init__.py
@@ -1,5 +1,6 @@
 """Eastmoney data access package."""
 
 from .EastmoneyController import EastmoneyController
+from .duokong_scanner import DuokongSnapshot, fetch_duokong_snapshot
 
-__all__ = ["EastmoneyController"]
+__all__ = ["EastmoneyController", "DuokongSnapshot", "fetch_duokong_snapshot"]
